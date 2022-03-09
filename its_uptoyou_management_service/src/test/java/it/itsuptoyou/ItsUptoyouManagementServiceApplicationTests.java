103fc4a7bac3386c2ab41cf561826cea075119d7
angelo <angelods18@gmail.com>
2022-03-09 21:47:21 +0100

    project creation

---
+++ its_uptoyou_management_service/src/test/java/it/itsuptoyou/ItsUptoyouManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsuptoyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsUptoyouManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
